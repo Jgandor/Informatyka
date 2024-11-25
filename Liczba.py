58632c80cb7ca2a797ade727b341ee030722e941
Jgandor <janek.gandor@gmail.com>
2024-11-25 11:50:56 +0100

    Dodano punktacje w Liczba.py

--- Liczba.py
+++ Liczba.py
@@ -8,19 +8,26 @@ parser.add_argument(
 )
 args = parser.parse_args()
 print("Hint: 0 to quit")
+s = 0
 while True:
     x = random.randint(2, 9)
     y = random.randint(2, 9)
     start = time.time()
     z = int(input(f"ile to {x} razy {y}? "))
     if z == 0:
-        exit()
+        q = int(input("Twój wynik to:", s "Punktów. Aby zakończyć wcisnij 0"))
+        if q == 0:
+            exit()
     end = time.time()
     if x * y == z:
+        s = s + 2
         print("dobrze")
         if end - start > args.limit:
+            s = s - 1
             print(f"ale za długo ({end-start:.2f} sekund)")
     else:
+        s = s - 1
         print(f"źle, powinno być", x * y)
         if end - start > args.limit:
+            s = s - 1
             print(f"i za długo ({end-start:.2f} sekund)")
