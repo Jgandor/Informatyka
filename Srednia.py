c941ffaa1101bcf8c056935eb066091c3d4cddf6
Jgandor <janek.gandor@gmail.com>
2024-11-25 08:00:46 +0100

    Zmiany w średniej. właśćiwie była robione od zera

--- Srednia.py
+++ Srednia.py
@@ -1,1 +1,4 @@
-a = int(input("Podaj pierwsza liczbe:
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj pierwszą liczbę: "))
+c = a * b / 2
+print("Średnia artmetyczna tych licz wynosi: ", c)
